f64130efa90467c8b87cc09565a6af6a2d88d440
Oleh Kurpiak <okurpiak@l320st02.di.lan>
2018-03-05 20:02:48 +0200

    fibonachi recurtion with cache

--- basic/recursion/src/Main.java
+++ basic/recursion/src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        int n = 10;
+        int[] cash = new int[n+1];
+        System.out.println(fibo(n, cash));
     }
+
+    public static int fibo (int n, int[] cash) {
+
+        if (n ==1 ) return 1;
+        if (n==0) return 0;
+
+        if ( cash[n] != 0) {
+            return cash[n];
+        }
+
+        cash[n] = fibo(n-1, cash) + fibo(n-2, cash);
+        return cash[n];
+    }
+
 }
