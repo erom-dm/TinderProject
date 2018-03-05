e769441f38d47386a8a30c08f897549c3d349039
Oleh Kurpiak <okurpiak@l320st02.di.lan>
2018-03-05 22:11:18 +0200

    triangle solve

--- basic/recursion/src/Main.java
+++ basic/recursion/src/Main.java
@@ -3,7 +3,13 @@ public class Main {
     public static void main(String[] args) {
         int n = 10;
         int[] cash = new int[n+1];
-        System.out.println(fibo(n, cash));
+//        System.out.println(fibo(n, cash));
+
+        for (int value : cash) {
+//            System.out.print(value + " ");
+        }
+//        System.out.println();
+        System.out.println(mystery(3,50));
     }
 
     public static int fibo (int n, int[] cash) {
@@ -19,4 +25,12 @@ public class Main {
         return cash[n];
     }
 
+
+    // detect problem
+    public static long mystery(long a, long b) {
+        if (b == 0)     return 1;
+        if (b % 2 == 0) return mystery(a*a, b/2);
+        System.out.println("set");
+        return mystery(a*a, b/2) * a;
+    }
 }
