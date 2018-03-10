36109d9cd39f65bb89ee80ca1807ff62eadd3778
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2018-03-10 10:43:58 +0200

    remove second

---
+++ basic/linke_list/src/com/danit/Main.java
@@ -0,0 +1,26 @@
+package com.danit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node (2);
+        head.next.next = new Node(3);
+
+        removeNext(head);
+    }
+
+    static class Node {
+        int val;
+        Node next;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void removeNext(Node head) {
+        head.next = head.next.next;
+    }
+
+}
