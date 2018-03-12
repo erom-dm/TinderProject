f6c202f72a1c1253bc5002db818b3a68ce3bcc38
Oleh Kurpiak <okurpiak@l320st04.di.lan>
2018-03-12 20:31:40 +0200

    first unit bit & count bits

---
+++ basic/bits/src/com/danit/FirstUnitBit.java
@@ -0,0 +1,23 @@
+package com.danit;
+
+import java.util.Scanner;
+
+public class FirstUnitBit {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        if(number ==0){
+            System.out.println(number);
+            return ;
+        }
+        int counter = 1;
+
+        while ((number & 1)==0) {
+            number=number>>1;
+            counter++;
+
+
+        }
+        System.out.println(counter);
+    }
+}
