b9d33e2dad16d8545a2bb4722b068189c1acc55b
borsch <oleh.kurpiak@gmail.com>
2018-03-19 21:57:14 +0200

    string length histogram

---
+++ basic/binary_search/src/com/danit/StringlengthHistogram.java
@@ -0,0 +1,38 @@
+package com.danit;
+
+public class StringlengthHistogram {
+
+    public static void main(String[] args) {
+        histogram("Lorem ipsum dolor sit amet");
+    }
+
+    public static void histogram(String input) {
+        input = input.replace("  ", " ");
+        String[] parts = input.split(" ");
+
+        int max = parts[0].length();
+        for (int i = 0; i < parts.length; ++i) {
+            max = Math.max(max, parts[i].length());
+        }
+
+        int[] length = new int[max + 1];
+        for (String part : parts) {
+            length[part.length()]++;
+        }
+
+        int maxInArray = length[0];
+        for (int elem : length) {
+            maxInArray = Math.max(elem, maxInArray);
+        }
+
+        for (int j = maxInArray; j > 0; --j){
+            for (int i = 0; i < length.length; ++i) {
+                if (length[i] >= j) {
+                    System.out.print("| ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
