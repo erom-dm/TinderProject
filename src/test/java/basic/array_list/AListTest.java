fb19c9dfeddc6d3554e28441995a3a540dbdcc62
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2018-03-30 22:27:02 +0300

    AList with Generics with tests and equals()

--- src/test/java/basic/array_list/AListTest.java
+++ src/test/java/basic/array_list/AListTest.java
@@ -7,74 +7,71 @@ public class AListTest {
 
     @Test
     public void addTest1() {
-        AList<Integer> list = new AList <Integer> (10);
+        AList<Integer> list = new AList <> (10);
         list.add(10);
         list.add(15);
         list.add(20);
-
         assertEquals(Integer.valueOf(10), list.get(0));
         assertEquals((Integer) 15, list.get(1));
         assertEquals(new Integer(20), list.get(2));
     }
 
     @Test
-    public void resizeTest(){
-        AList list = new AList(10);
+    public void resizeTest() {
+        AList<Integer> list = new AList<>(10);
         for (int i = 0; i < list.size(); i++) {
             list.add(i*12);
         }
-
         list.add(133);
-
-        //assertEquals(133, list.get(10));
-    }
-    @Test
-    public void otherTest(){
-        AList list = new AList(10);
-        for (int i = 0; i < list.size(); i++) {
-            list.add(i*12);
-        }
-
-        list.add(133);
-
-        //assertEquals(133, list.get(10));
+        assertEquals(Integer.valueOf(133), list.get(10));
     }
 
     @Test
-    public void zeroTest(){
-        AList list = new AList(10);
+    public void otherTest() {
+        AList<Integer> list = new AList<>(10);
         for (int i = 0; i < list.size(); i++) {
             list.add(i*12);
         }
+        list.add(133);
+        assertEquals(Integer.valueOf(133), list.get(10));
+    }
 
+    @Test
+    public void zeroTest() {
+        AList<Integer> list = new AList<>(10);
+        for (int i = 0; i < list.size(); i++) {
+            list.add(i*12);
+        }
         assertEquals(null, list.get(15));
     }
 
     @Test
-    public void emptyValueTest(){
-        AList list = new AList(10);
+    public void emptyValueTest() {
+        AList<Integer> list = new AList<>(10);
         for (int i = 0; i < list.size(); i++) {
             list.add(i*12);
         }
-
         assertEquals(null, list.get(15));
 
-    }    @Test
+    }
+
+    @Test
     public void emptyValueTestString(){
-        AList<String> list = new AList<String>(10);
+        AList<String> list = new AList<>(10);
         for (int i = 0; i < list.size(); i++) {
             list.add("Name " + i);
         }
-
         assertEquals("Name 0", list.get(0));
     }
+
     @Test
     public void emptyValueTestAlist(){
         AList<AList<String>> list = new AList<>(10);
         for (int i = 0; i < list.size(); i++) {
             list.add(new AList<String>(i));
         }
-
-        assertEquals(new AList<>(0), list.get(0));
+        AList<String> expected = new AList<>(0);
+        AList<String> real = list.get(0);
+        assertEquals(expected, real);
     }
 }
