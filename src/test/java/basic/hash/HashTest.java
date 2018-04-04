4f55a1b6f0fc30bb2ffc68dd5ae0ee6034f0a2d8
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2018-04-04 23:14:51 +0300

    hash with collision resolved

--- src/test/java/basic/hash/HashTest.java
+++ src/test/java/basic/hash/HashTest.java
@@ -6,8 +6,11 @@ import static org.junit.Assert.*;
 
 public class HashTest {
     @Test
-    public void test1() {
+    public void testCollisionsResolved() {
         Hash hash = new Hash();
+        hash.put(10,"10");
+        hash.put(99,"99");
+        hash.put(999,"999");
         hash.put(15,"ABC");
         hash.put(123,"HELLO");
         hash.put(217,"CAT");
@@ -17,6 +20,9 @@ public class HashTest {
         assertEquals("ABC", hash.get(15));
         assertEquals("HELL ;)", hash.get(999999157));
         assertEquals("CAT", hash.get(217));
+        assertEquals("10", hash.get(10));
+        assertEquals("99", hash.get(99));
+        assertEquals("999", hash.get(999));
     }
 
 }
