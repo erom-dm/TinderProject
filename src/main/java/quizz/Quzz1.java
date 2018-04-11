532e342893dd2c76f42754c15ca5429e33a91505
arykhalskiy <alexey.rykhalskiy@gmail.com>
2018-04-11 21:53:58 +0300

    Quzz1

---
+++ src/main/java/quizz/Quzz1.java
@@ -0,0 +1,11 @@
+package quizz;
+
+public class Quzz1 {
+    public static void main(String[] args) {
+        String s="Apple "+
+                "costs "
+                + '2' +
+                "UAH";
+        System.out.println(s);
+    }
+}
