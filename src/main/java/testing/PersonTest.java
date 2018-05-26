53645eb868c69e83b905256da773a5f1e70ffc70
arykhalskiy <alexey.rykhalskiy@gmail.com>
2018-05-26 11:28:09 +0300

    testing

--- src/main/java/testing/PersonTest.java
+++ src/main/java/testing/PersonTest.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class PersonTest {
 
-
     public static void main(String[] args) {
         List<Person> people = new Database(25).get();
+        people.forEach(System.out::println);
     }
 }
