2cd59bfc30d4f5219fe2bb87c0bcbec7382de0b9
arykhalskiy <alexey.rykhalskiy@gmail.com>
2018-05-26 11:28:09 +0300

    testing

--- src/main/java/testing/Database.java
+++ src/main/java/testing/Database.java
@@ -2,10 +2,12 @@ package testing;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Database {
     private final String[] names = {"Alex", "Masha", "Dima", "Igor", "Sasha", "Lena"};
     private final String[] surNames = {"Petrenko", "Sydorenko", "Vinnichenko", "Shevchenko", "Moskalenko", "Savchenko"};
+    private final int arraySize = names.length;
     private final int quantity;
     private final int salaryMin = 10000;
     private final int salaryMax = 100000;
@@ -19,9 +21,10 @@ public class Database {
     }
 
     int generateValueFromRange(int min, int max) {
-
+        Random r = new Random();
+        int val = r.nextInt(max - min);
+        return min + val;
     }
-
 
     List<Person> get() {
         ArrayList<Person> people = new ArrayList<>();
@@ -32,6 +35,10 @@ public class Database {
     }
 
     private Person generateNewPerson() {
-        return null;
+        int id = generateValueFromRange(rangeMin, rangeMax);
+        String name = names[generateValueFromRange(0,arraySize)] + " " + surNames[generateValueFromRange(0,arraySize)];
+        int salary = generateValueFromRange(salaryMin, salaryMax);
+        int beginYear = generateValueFromRange(yearMin, yearMax);
+        return new PersonSimple(id, name, salary, beginYear);
     }
 }
---
+++ src/main/java/testing/PersonSimple.java
@@ -0,0 +1,40 @@
+package testing;
+
+public class PersonSimple implements Person {
+    private final int id;
+    private final String name;
+    private final int salary;
+    private final int beginYear;
+
+    public PersonSimple(int id, String name, int salary, int beginYear) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.beginYear = beginYear;
+    }
+
+    @Override
+    public int id() {
+        return this.id;
+    }
+
+    @Override
+    public String name() {
+        return this.name;
+    }
+
+    @Override
+    public int salary() {
+        return this.salary;
+    }
+
+    @Override
+    public int beginYear() {
+        return this.beginYear;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %-18s %d %d",this.id, this.name, this.salary, this.beginYear);
+    }
+}
