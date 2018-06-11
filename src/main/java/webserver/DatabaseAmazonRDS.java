6e25738e2497cde856e542cb23e3f506ecc338aa
arykhalskiy <alexey.rykhalskiy@gmail.com>
2018-06-11 21:41:37 +0300

    servlet

---
+++ src/main/java/webserver/DatabaseAmazonRDS.java
@@ -0,0 +1,13 @@
+package webserver;
+
+public class DatabaseAmazonRDS implements Database{
+    @Override
+    public void put(int k, String v) {
+
+    }
+
+    @Override
+    public String get(int k) {
+        return null;
+    }
+}
