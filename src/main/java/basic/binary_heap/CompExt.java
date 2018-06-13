2590feed29d17cc510adafd62024bc8647f9c79f
Erom <erom.mdn@gmail.com>
2018-06-13 19:48:21 +0300

    Initial Tinder Project commit.

--- src/main/java/basic/binary_heap/CompExt.java
+++
@@ -1,7 +0,0 @@
-package basic.binary_heap;
-
-import java.util.Comparator;
-
-public interface CompExt extends Comparator{
-    int compare22(int i1, int i2);
-}
--- src/main/java/basic/two_pointers/Main.java
+++
@@ -1,8 +0,0 @@
-package basic.two_pointers;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
