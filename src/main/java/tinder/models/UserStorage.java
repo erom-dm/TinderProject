e52194bfcc8d2f66302e861307acf05b722b414d
Erom <erom.mdn@gmail.com>
2018-06-16 17:29:37 +0300

    mid-way commit before liked-page implementation

--- src/main/java/tinder/models/UserStorage.java
+++ src/main/java/tinder/models/UserStorage.java
@@ -1,10 +1,35 @@
 package tinder.models;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class UserStorage implements Storage {
+public class UserStorage implements Storage, Iterable<User> {
     private final List<User> data = new ArrayList<>();
+
+    @Override
+    public Iterator<User> iterator() {
+        Iterator<User> it = new Iterator<User>() {
+
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < data.size() && data.get(currentIndex) != null;
+            }
+
+            @Override
+            public User next() {
+                return data.get(currentIndex++);
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+        return it;
+    }
 
     @Override
     public void put(User user) {
--- src/main/java/tinder/servlets/PeopleListServlet.java
+++ src/main/java/tinder/servlets/PeopleListServlet.java
@@ -4,9 +4,7 @@ import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
-import tinder.models.Database;
-import tinder.models.Opinion;
-import tinder.models.Storage;
+import tinder.models.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -20,9 +18,9 @@ import java.util.Map;
 
 public class PeopleListServlet extends HttpServlet {
     private final Database base;
-    private final Storage userStorage;
+    private final UserStorage userStorage;
 
-    public PeopleListServlet(tinder.models.Database base, tinder.models.Storage userStorage) {
+    public PeopleListServlet(tinder.models.Database base, UserStorage userStorage) {
         this.base = base;
         this.userStorage = userStorage;
     }
@@ -38,6 +36,8 @@ public class PeopleListServlet extends HttpServlet {
 
         Template template = cfg.getTemplate("people-list.html");
         resp.getWriter().write(template.toString());
-
+        /*for (User u : userStorage) {
+            u.
+        }*/
     }
 }
