5acbf01c318614db56fa93d00489ee136897cf80
erom-dm <32650878+erom-dm@users.noreply.github.com>
2018-06-18 16:42:25 +0300

    Create LoginServlet.java

---
+++ src/main/java/tinder/servlets/LoginServlet.java
@@ -0,0 +1,65 @@
+package tinder.servlets;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+import tinder.dao.UserStorage;
+import tinder.models.User;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class PeopleListServlet extends HttpServlet {
+    private final UserStorage userStorage;
+
+    public PeopleListServlet(UserStorage userStorage) {
+        this.userStorage = userStorage;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_28);
+        cfg.setDirectoryForTemplateLoading(new File("./src/main/java/tinder/templates"));
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        cfg.setLogTemplateExceptions(false);
+        cfg.setWrapUncheckedExceptions(true);
+
+        Map<String, Object> model = new HashMap<>();
+        List<User> likedUsers = new ArrayList<>();
+        for (User u : userStorage) {
+            if(u.isLiked()){
+                likedUsers.add(u);
+            }
+        }
+        model.put("likedUsers", likedUsers);
+
+        Template template = cfg.getTemplate("people-list.html");
+        Writer out = resp.getWriter();
+
+        try {
+            template.process(model, out);
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("backButton");
+        if(name.equals("back")){
+            resp.sendRedirect("/users");
+        }
+    }
+}
