0016145ea657b20beb05f1aa3dd66cf9f8260514
erom-dm <PArkerr1alto?>
2018-06-18 22:40:52 +0300

    connection to remote db tweaks

--- src/main/java/tinder/dao/ConnectionToDB.java
+++ src/main/java/tinder/dao/ConnectionToDB.java
@@ -3,7 +3,11 @@ package tinder.dao;
 import java.sql.*;
 
 public class ConnectionToDB {
-    private static final String DB_URL = "mysql://danit.cukm9c6zpjo8.us-west-2.rds.amazonaws.com:3306/fs5";
+    //5432 port postgres
+    //3306 port mysql
+    //"jdbc:postgresql";
+    //"jdbc:mysql";
+    private static final String DB_URL = "jdbc:mysql://danit.cukm9c6zpjo8.us-west-2.rds.amazonaws.com:3306/fs5";
     private static final String USERNAME = "fs5_user";
     private static final String USER_PASS = "bArceloNa";
 
